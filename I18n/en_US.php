b8e5e25605bd68ec76837a991c8a486c5a60b65a
Michaël Espeche <michael.espeche@gmail.com>
2014-02-14 14:29:20 +0100

    Add translations

---
+++ I18n/en_US.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    '[Tabs]Tabs' => 'Tabs',
+    '[Tabs]One of the tabs that you have changed or created contains one or more errors. The <strong>Title</strong> and <strong>Description</strong> fields are mandatory' => 'One of the tabs that you have changed or created contains one or more errors. The <strong>Title</strong> and <strong>Description</strong> fields are mandatory',
+    '[Tabs]Tabs association' => 'Tabs association',
+    '[Tabs]Tab created on %date_create. Last modification: %date_change' => 'Tab created on %date_create. Last modification: %date_change',
+    '[Tabs]Delete this tab' => 'Delete this tab',
+    '[Tabs]Delete tab' => 'Delete tab',
+    '[Tabs]Do you really want to delete this tab ?' => 'Do you really want to delete this tab ?'
+);
---
+++ I18n/fr_FR.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    '[Tabs]Tabs' => 'Onglets',
+    '[Tabs]One of the tabs that you have changed or created contains one or more errors. The <strong>Title</strong> and <strong>Description</strong> fields are mandatory' => 'Un des onglets que vous avez créé ou modifié contient une ou plusieurs erreurs. Les champs <strong>Titre</strong> et <strong>Description</strong> sont obligatoires',
+    '[Tabs]Tabs association' => 'Onglets associés',
+    '[Tabs]Tab created on %date_create. Last modification: %date_change' => 'Onglet créé le %date_create. Dernière modification le %date_change',
+    '[Tabs]Delete this tab' => 'Supprimer cet onglet',
+    '[Tabs]Delete tab' => 'Suppression d\'onglet',
+    '[Tabs]Do you really want to delete this tab ?' => 'Voulez-vous vraiment supprimer cet onglet ?'
+);
